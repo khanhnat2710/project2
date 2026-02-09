01b3c3b93155657aba93b29253acbdd39196a697
khanhnat2710 <khanhnat2710@gmail.com>
2026-02-09 15:51:11 +0700

    day code

--- app/Models/Admin/foodInvoiceDetail.php
+++ app/Models/Admin/foodInvoiceDetail.php
@@ -6,5 +6,26 @@ use Illuminate\Database\Eloquent\Model;
 
 class foodInvoiceDetail extends Model
 {
-    //
+    protected $table = 'foodInvoiceDetail';
+
+    // composite primary key
+    protected $primaryKey = null;
+    public $incrementing = false;
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'foodInvoiceID',
+        'foodID',
+        'quantity'
+    ];
+    public function foodInvoice()
+    {
+        return $this->belongsTo(FoodInvoice::class, 'foodInvoiceID');
+    }
+
+    public function food()
+    {
+        return $this->belongsTo(Food::class, 'foodID');
+    }
 }
--- app/Models/Admin/genreDetail.php
+++ app/Models/Admin/genreDetail.php
@@ -6,5 +6,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class genreDetail extends Model
 {
-    //
+    protected $table = 'genreDetail';
+
+    // composite key → Laravel không tự xử lý
+    protected $primaryKey = null;
+    public $incrementing = false;
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'movieID',
+        'genreID'
+    ];
+    public function movie()
+    {
+        return $this->belongsTo(Movie::class, 'movieID');
+    }
+
+    public function genre()
+    {
+        return $this->belongsTo(Genre::class, 'genreID');
+    }
 }
