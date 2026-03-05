d85f2a07edfa6b8d23e33357224a61085f36a8e7
duonghud <chiuroi2312@gmail.com>
2026-03-05 14:58:11 +0700

    update1

--- resources/views/customer/edit.blade.php
+++ resources/views/customer/edit.blade.php
@@ -1,40 +1,51 @@
 @extends('layouts.appAdmin')
 
 @section('content')
-    <form method="post" action="{{ route('customer.update', $customers->customerID) }}">
-        @csrf
-        @method('PUT')
 
-        <div>
-            <label for="fullName">Họ tên: </label>
-            <input type="text" name="fullName" id="fullName" value="{{ $customers->fullName }}">
-        </div>
+    <div class="card-body">
+        <form method="post" action="{{ route('customer.update', $customers->customerID) }}">
+            @csrf
+            @method('PUT')
 
-        <div>
-            <label for="email">Email: </label>
-            <input type="text" name="email" id="email" value="{{ $customers->email }}">
-        </div>
+            <div class="mb-3">
+                <label for="fullName" class="form-label">Họ tên</label>
+                <input type="text" class="form-control" name="fullName" id="fullName"
+                    value="{{ $customers->fullName }}">
+            </div>
 
-        <div>
-            <label>Password mới (nếu muốn đổi):</label>
-            <input type="password" name="password">
-        </div>
+            <div class="mb-3">
+                <label for="email" class="form-label">Email</label>
+                <input type="text" class="form-control" name="email" id="email"
+                    value="{{ $customers->email }}">
+            </div>
 
-        <div>
-            <label for="phoneNumber">Số điện thoại: </label>
-            <input type="text" name="phoneNumber" id="phoneNumber" value="{{ $customers->phoneNumber }}">
-        </div>
+            <div class="mb-3">
+                <label class="form-label">Password mới (nếu muốn đổi)</label>
+                <input type="password" class="form-control" name="password">
+            </div>
 
+            <div class="mb-3">
+                <label for="phoneNumber" class="form-label">Số điện thoại</label>
+                <input type="text" class="form-control" name="phoneNumber" id="phoneNumber"
+                    value="{{ $customers->phoneNumber }}">
+            </div>
 
-        <div>
-            <label for="address">Địa chỉ: </label>
-            <input type="text" name="address" id="address" value="{{ $customers->address }}">
-        </div>
+            <div class="mb-3">
+                <label for="address" class="form-label">Địa chỉ</label>
+                <input type="text" class="form-control" name="address" id="address"
+                    value="{{ $customers->address }}">
+            </div>
 
-        <div>
-            <button type="submit" class="btn btn-primary">
-                Cập nhật
-            </button>
-        </div>
-    </form>
-@endsection
+            <div class="d-flex justify-content-between">
+                <a href="{{ route('customer.index') }}" class="btn btn-secondary">
+                    Quay lại
+                </a>
+
+                <button type="submit" class="btn btn-primary">
+                    Cập nhật
+                </button>
+            </div>
+
+        </form>
+    </div>
+</div>
